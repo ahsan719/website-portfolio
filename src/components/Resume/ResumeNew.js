34f54549d3e447a5cc77e1da4494cd79367666a8
Ahsan Naseer <ahsannilore@gmail.com>
2025-05-17 20:30:58 +0500

    updated CV

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV_Latest.pdf";
+import pdf from "../../Assets/../Assets/CV__Ahsan.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
