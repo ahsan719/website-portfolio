5f47f76a03354163811cf9ad7edd1a2b0b8e61ed
Ahsan Naseer <ahsannilore@gmail.com>
2025-05-24 09:28:38 +0500

    Info updated

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,11 +4,7 @@ import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-  AiFillLinkedin,
   AiFillFacebook
-
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
@@ -22,37 +18,37 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I fell in love with programming — and over time, I’ve built
+              real-world solutions in both academic and applied settings. From AI
+              projects to cloud deployments, I continue to grow with every line
+              of code I write. 🤷
               <br />
-              <br />I am fluent in classics like
+              <br />
+              I am fluent in classics like
               <i>
-                <b className="purple"> C++. python and Javascript </b>
+                <b className="purple"> C++, Python, and JavaScript </b>
+              </i>
+              and have applied them in areas like IoT, HPC, and security automation.
+              <br />
+              <br />
+              My field of interest includes developing impactful&nbsp;
+              <i>
+                <b className="purple">Web Technologies</b> and{" "}
+                <b className="purple">AI-based products</b>, especially those that
+                empower underserved communities and enhance system automation.
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Artificial Intelligence.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
+              Whenever possible, I apply my passion to building real-world tools
               with <b className="purple">Node.js</b> and
               <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
+                <b className="purple"> modern JavaScript libraries and frameworks</b>
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> React.js </b>
               </i>
+              .
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -98,16 +94,6 @@ function Home2() {
                   <FaLinkedinIn />
                 </a>
               </li>
-              {/* <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li> */}
             </ul>
           </Col>
         </Row>
@@ -115,4 +101,5 @@ function Home2() {
     </Container>
   );
 }
+
 export default Home2;
